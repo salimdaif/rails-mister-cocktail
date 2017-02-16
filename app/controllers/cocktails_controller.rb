3b4d6573f504193b8e21aaf53b4d9f571ad0fa6d
Salim Daif <salim.daif@gmail.com>
2017-02-16 14:38:19 -0200

    Mister Cocktail WORKS

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,27 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(params_cocktail)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def params_cocktail
+    params.require(:cocktail).permit(:name)
   end
 end
